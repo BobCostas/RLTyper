98047a8f418bda9ccdc24d390e8fdc707f545452
Bob Costas <finkzane@gmail.com>
2017-07-29 18:55:50 -0700

    Testing ConfigManager

--- src/RLTyper.java
+++ src/RLTyper.java
@@ -1,5 +1,28 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 public class RLTyper {
-    public static void main(String[] args) {
-        System.out.println("Nice meme");
-    }
+        public static void main(String[] args) throws FileNotFoundException {
+                ConfigManager cf = new ConfigManager();
+
+                cf.loadConfig();
+            HashMap<String, ArrayList<String>> map = new HashMap<>();
+            ArrayList<String> stringArrayList = new ArrayList<>();
+            stringArrayList.add("Meme1");
+            stringArrayList.add("Meme2");
+            stringArrayList.add("Meme3");
+            stringArrayList.add("Meme4");
+            map.put("North", stringArrayList);
+            //Scanner scan=new Scanner(file);
+            System.out.println(cf.formatConfigFileEntry(map));
+        }
+
+
 }
