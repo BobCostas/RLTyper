0308b3d38f6096fb57909b62885f8410570e5f54
Bob Costas <finkzane@gmail.com>
2017-08-03 13:33:51 -0700

    Added method to write configs to a file

--- src/ConfigManager.java
+++ src/ConfigManager.java
@@ -1,8 +1,6 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -34,45 +32,41 @@ public class ConfigManager {
 
 
     /**
-     *
      * @param configMap A hashmap containing String as the key, and an ArrayList of Strings
      *                  as the values. The key is the name of our direction
      *                  e.g. North, East, South, or West, and the ArrayList
      *                  contains the Messages associated with that direction
      * @return A String formatted as "Direction: [Thing1][Thing2][Thing3][Thing4]"
      */
-   public String formatConfigAsString(HashMap<String, ArrayList<String>> configMap) {
-     String outputString = "";
-     // Loop through each of the keys in our HashMap
-     for (String key : configMap.keySet()) {
-        outputString += key + ": ";
-        for (String string : configMap.get(key)) {
-            outputString += "[" + string + "]";
-         }
+    public String formatConfigAsString(HashMap<String, ArrayList<String>> configMap) {
+        String outputString = "";
+        // Loop through each of the keys in our HashMap
+        for (String key : configMap.keySet()) {
+            outputString += key + ": ";
+            for (String string : configMap.get(key)) {
+                outputString += "[" + string + "]";
+            }
 
-     }
+        }
 
 
         return outputString;
-   }
-
-    /**
-     * Creates an entry for the config file, one direction of our D-pad input
-     * @param configMap A hashmap containg String as the key, and an ArrayList of Strings
-     */
-  //  public void createConfigFileEntry(HashMap<String, ArrayList<String>> configMap) {
-       // Load the default CharSet
-//       Charset charset = Charset.defaultCharset();
-
-       //create our buffered Writer, which writes our value to the file
-
- //   }
+    }
 
 
     /**
-     * TODO See if this is the right class to implement this in
-     * @return
+     * Writes a config to the file specified in the format created by formatConfigAsString
+     *
+     * @param configToWrite the data we are going to write to the file, a hashmap of an arraylist containing strings,
+     *                      see formatAsConfig for more information
+     * @param fileToWriteTo The file we are going to write to, if it already exists it is overwritten,
+     *                      if not then it is created
      */
-    public boolean checkMessageLength() {return true;}
-
+    public void writeConfigToFile(HashMap<String, ArrayList<String>> configToWrite, File fileToWriteTo) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter(fileToWriteTo);
+        // Formats our input data, and writes it to the file
+        out.println(formatConfigAsString(configToWrite));
+        // Close the file, clear our buffer
+        out.close();
+    }
 }
