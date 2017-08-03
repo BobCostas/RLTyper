51af1e7828bbca87eeb7bfb0dd2b724b93b6c4e1
Bob Costas <finkzane@gmail.com>
2017-08-02 19:08:53 -0700

    Added functionality to format a config as A String

--- src/ConfigManager.java
+++ src/ConfigManager.java
@@ -1,57 +1,55 @@
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class ConfigManager {
     /**
      * ConfigManager interacts with configs, from creating, loading,
      */
-    Path file = Paths.get("/home/zane/Documents/Programming/Java/Projects/RLTyper/src/sampletext");
 
     /**
      * Loads a config from a config file
      * TODO Implement of a config in the correct format
      */
-    public void loadConfig() {
-        // Load the default charset
-        Charset charset = Charset.defaultCharset();
+    public ArrayList<String> loadConfigFromFile(File inputFile) throws FileNotFoundException {
+        ArrayList<String> returnArrayList = new ArrayList<>();
+        // Create a scanner to read our inputFile
+        Scanner in = new Scanner(inputFile);
+        // Loop through each line, and add it to our output ArrayList
+        while (in.hasNextLine()) {
+            returnArrayList.add(in.nextLine());
+        }
+        // Remember to close the file
+        in.close();
+        return returnArrayList;
 
-        // Create the buffered reader, read the config file from it
-        try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException x) {
-                System.err.format("IOException %s %n", x);
-                }
+
     }
 
 
     /**
      *
-     * @param configMap A hahmap containing String as the key, and an ArrayList of Strings
+     * @param configMap A hashmap containing String as the key, and an ArrayList of Strings
      *                  as the values. The key is the name of our direction
      *                  e.g. North, East, South, or West, and the ArrayList
      *                  contains the Messages associated with that direction
-     * @return A String formatted as "Direction: [item1, item2, item3, item4]\n"
+     * @return A String formatted as "Direction: [Thing1][Thing2][Thing3][Thing4]"
      */
-   public String formatConfigFileEntry(HashMap<String, ArrayList<String>> configMap) {
+   public String formatConfigAsString(HashMap<String, ArrayList<String>> configMap) {
      String outputString = "";
+     // Loop through each of the keys in our HashMap
      for (String key : configMap.keySet()) {
         outputString += key + ": ";
-        outputString += "[";
         for (String string : configMap.get(key)) {
-            outputString += string + ", ";
-
+            outputString += "[" + string + "]";
          }
-         outputString += "]";
+
      }
 
 
@@ -69,6 +67,7 @@ public class ConfigManager {
        //create our buffered Writer, which writes our value to the file
 
  //   }
+
 
     /**
      * TODO See if this is the right class to implement this in
