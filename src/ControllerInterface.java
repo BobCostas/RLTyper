fcde8ee1a42fcb93e2a6d936682a4fb11cf5a176
Bob Costas <finkzane@gmail.com>
2017-08-09 17:11:35 -0700

    Added StringerSender class to interface with Robot

--- src/ControllerInterface.java
+++ src/ControllerInterface.java
@@ -1,22 +1,76 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
+class StringSender extends Robot {
+    private int sleepTime;
+
+    public StringSender(int defaultSleepTime) throws AWTException {
+        super();
+        this.sleepTime = defaultSleepTime;
+    }
+
+    public StringSender() throws AWTException {
+        this(0);
+    }
+
+     public int getSleepTime() {
+        /**
+         * Returns the time the the robot sleeps between sending characters, in ms
+         */
+        return this.sleepTime;
+    }
+
+    /**
+     * Sets the time, in ms for the the robot to sleep in between typing characters
+     * @param newSleepTime The amount of time you want the the robot to sleep
+     *                     in between typing characters
+     */
+    public void setSleepTime(int newSleepTime) {
+
+        this.sleepTime = newSleepTime;
+    }
+
+    /**
+     * Uses the robot to type a string, and sleeps a certain amount between keypresses
+     *
+     * @param stringToType
+     * @throws AWTException
+     */
+    public void typeString(String stringToType) throws AWTException {
+
+        for (char ch : stringToType.toCharArray()) {
+            int charToSend = KeyEvent.getExtendedKeyCodeForChar(ch);
+            if (Character.isUpperCase(ch)) {
+                this.keyPress(KeyEvent.VK_SHIFT);
+            }
+            this.keyPress(charToSend);
+            this.keyRelease(charToSend);
+            this.keyRelease(KeyEvent.VK_SHIFT);
+            this.delay(getSleepTime());
+
+
+        }
+    }
+}
+
+
 public class ControllerInterface {
     /**
      * ControllerInterface uses JInput in order to find a valid controller,
      * and listens for D-pad input in order to send strings
-      */
+     */
 
     // The rate at which JInput will listen for inputs
     private final int POLLING_RATE;
-    private int sleepTime;
+    private StringSender ss = new StringSender();
+
 
     // Set polling rate, if not specified default is 200ms
-    public ControllerInterface(int POLLING_RATE) {
+    public ControllerInterface(int POLLING_RATE) throws AWTException {
         this.POLLING_RATE = POLLING_RATE;
     }
 
-    public ControllerInterface() {
+    public ControllerInterface() throws AWTException {
         this.POLLING_RATE = 200;
     }
 
@@ -30,27 +84,8 @@ public class ControllerInterface {
                 "has been set.";
     }
 
-    public int getSleepTime() {
-        /**
-         * Returns the time the robot sleeps between sending characters, in ms
-         */
-        return this.sleepTime;
+    public void type(String messageToType) throws AWTException {
+        ss.typeString(messageToType);
     }
 
-    public void setSleepTime(int newSleepTime) {
-        /**
-         * Sets the time, in ms for the robot to sleep in between typing characters
-         */
-        this.sleepTime = newSleepTime;
-    }
-
-    public void typeString(String stringToType) throws AWTException{
-        Robot rob = new Robot();
-        for (char ch : stringToType.toCharArray()) {
-            rob.keyPress(KeyEvent.VK_);
-            rob.delay(getSleepTime());
-
-
-        }
-    }
 }
